7acac97ffc068c4b88a0287fc3c9403f9482620b
raff1100 <118704958+raff1100@users.noreply.github.com>
2024-12-02 02:29:21 +0530

    todolist commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#f1f1f1', margin:'auto' }}>
+      <p>© 2024 Daily Goal. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,6 @@
+import React from 'react'
+const Header = () => {
+  return <nav>Get Ready to complete Your Daily Goals!</nav>
+}
+
+export default Header
